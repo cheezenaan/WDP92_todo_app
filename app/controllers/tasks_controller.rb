0e5124fa65d79a5164bac04d306e1085eafaa353
cheezenaan <cheezenaan@gmail.com>
2016-08-29 07:59:00 +0900

    Add Tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,67 @@
+class TasksController < ApplicationController
+
+  # GET /tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # GET tasks/:id
+  def show
+
+  end
+
+  # GET tasks/new
+  def new
+    @task = Task.new
+  end
+  
+  # GET tasks/id/edit
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.json { render show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.json { render show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destory
+    @task.destroy
+    respond_to do |format|
+      format_html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format_json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+
+end
+
